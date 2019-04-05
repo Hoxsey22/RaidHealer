987f47dedaffc1d161337b2329199515880cccac
Joseph Hoxsey <JosephHoxsey@gmail.com>
2019-04-05 02:39:37 -0700

    - Changed the version to 1.01 - Changed the tutorial text.

--- core/src/com/hoxseygames/raidhealer/TutorialFrame.java
+++ core/src/com/hoxseygames/raidhealer/TutorialFrame.java
@@ -52,7 +52,7 @@ public class TutorialFrame extends Group {
         getTable().setBounds(RaidHealer.WIDTH/2 -150,RaidHealer.HEIGHT/2-150,300,300);
 
 
-        setText(new Text("Welcome Healer! This is the tutorial on how to be a healer so pay close attention!",24, Color.WHITE, false, getAssets()));
+        setText(new Text("Welcome healer! This tutorial will show you how to be a healer!",24, Color.WHITE, false, getAssets()));
         getText().setWrap();
         getText().setAlignment(Align.center);
 
@@ -66,43 +66,52 @@ public class TutorialFrame extends Group {
         setStageNumber(getStageNumber() + 1);
         switch(getStageNumber())    {
             case 2:
-                getText().setText("This is the Boss Frame. When the health bar is completely empty or red then the boss has been defeated!");
+                getText().setText("This is the Boss's health bar, you must keep your raid alive until the Boss's health bar is depleted.");
                 break;
-            case 3:
-                getText().setText("This is the announcement section. If a special ability is coming, it will be announced here. You will have about 1.5 seconds to prepare for the ability.");
+            case 3:                         // When a special ability is coming, it will state what ability is coming and you will have 1.5 seconds to prepare for said ability.
+                getText().setText("This is the announcement section. When a special ability is coming, it will state what ability is coming and you will have 1.5 seconds to prepare for said ability.");
                 break;
-            case 4:
-                getText().setText("These are the raid frames. These are the ally units you will be protecting so watch them closely!\n" +
-                        "There are three different types of Ally Units; Tanks, DPS, and Healers. \n");
+            case 4:                 // These are your raid frames and each frame is an ally unit you needs to protect so watch them closely!
+                getText().setText("These are your raid frames and each frame is an ally unit you needs to protect so watch them closely!\n" +
+                        "There are three different types of ally units; Tanks, DPS, and Healers. \n");
                 break;
             case 5:
-                getText().setText("The Tank is the primary target for the boss and will take constant damage. Watch them carefully. The tank has double the health of the other units, 200 hp to be exact, and the Tank can do about 5 damage per second.");
+                getText().setText("The Tank is the primary target for the boss and will normally take constant damage. The tank has double the health of the other ally units, 200 hp to be exact, and the Tank will do 5 damage per second.");
                 break;
             case 6:
-                getText().setText("These are the healers, your brethren. Healers provide you with 2 mana per second and will deal 2 damage per second. Protect your brethren as they provide you and your raid with damage and mana back.");
+                getText().setText("These are the Healers, your brethren. Healers provide you with 2 mana per second and will deal 2 damage per second.");
                 break;
             case 7:
-                getText().setText("The DPS also known as the damage dealers, are the primary source of damage toward the boss. They deal 10 damage per second so they are crucial to keep alive.");                break;
+                getText().setText("The DPS, also known as the damage dealers, are your primary source of damage dealing 10 damage per second.");
+                break;
             case 8:
-                getText().setText("These are the buffs and debuffs. Buffs are positive benefits and Debuffs are negative benefits for the ally unit. ");
+                getText().setText("These are the buffs and debuffs. Buffs are positive benefits and Debuffs are negative benefits for your ally units. ");
                 break;
             case 9:
-                getText().setText("Now the red box around the ally unit indicates that this particular unit is being targeted by the boss and should be watched at all times because all bosses have an auto attack.");
+                getText().setText("The red box around the ally unit indicates that this particular unit is being targeted by the boss.");
                 break;
             case 10:
-                getText().setText("In order to target a unit, you must touch the frame you want to select. The frame will turn light blue indicating the target has been selected.");
+                getText().setText("In order to target an ally unit, you must touch the frame of the ally unit you want to select. The frame will turn light blue indicating the ally unit has been selected.");
                 break;
-            case 11:
-                getText().setText("When a target is selected, you may use any of the spells in your bar indicated in the green. To do so, just press the spell and the spell will use its ability to heal the selected unit. Each spell has a different ability and are used for different situations, but you can only have 4 spells at once so choose wisely.");
+            case 11:                                                // you can now use any of the spells in your spell bar.
+                getText().setText("When an ally unit is selected, you can now use any of the spells in your spell bar. " +
+                        // Just press the spell and the spell's ability will be triggered on the selected ally unit.
+                        "Just press the spell and the spell's ability will be triggered on the selected ally unit. " +
+                        // Each spell has it's own unique ability and should be should for certain situations. You are limited to 4 spells only.
+                        "Each spell has it's own unique ability and should be used for certain situations. You are limited to only 4 spells.");
                 break;
             case 12:
-                getText().setText("Each spell costs mana in order to cast the spell. The bar in the green box indicates your mana. You only have 1000 mana so be cautious. ");
+                                    // Spells can only be used if you have enough mana. This is your mana bar which contains 1000 mana as indicated.
+                getText().setText("Spells can only be used if you have enough mana. " +
+                        "This is your mana bar which contains 1000 mana as indicated.");
                 break;
-            case 13:
-                getText().setText("Some spells also have a cast time. The bar in the green is your cast bar and when the cast bar is complete, the spell’s ability will be applied to the unit.");
+            case 13:                // Certain spells have to be casted before the ability is used. The cast bar shows the progression of the spell cast and when the cast is complete the ability will be triggered.
+                getText().setText("Certain spells have to be casted before the ability is used. " +
+                        "The cast bar shows the progression of the spell cast and when the cast is complete the ability will be triggered.");
                 break;
             case 14:
-                getText().setText("Now that you know all the essentials for being a healer, let’s put it to the test and start your journey…");
+                getText().setText("These are essentials for being a healer and you will learn more as you progress." +
+                        "\nNow let's test your healing skills and start your journey…");
                 break;
             case 15:
                 getText().setText("Can you save your raid before it’s too late?");
